9c66ed804d24320e6eee5c9faf94c6169b49b039
Andreas Heissenberger <andreas@heissenberger.at>
2025-08-26 02:45:33 +0200

    fix: update module declaration for 'vite-plugin-node-worker' to export workerPlugin function

--- vite-plugin-node-worker.d.ts
+++ vite-plugin-node-worker.d.ts
@@ -2,7 +2,8 @@ declare module "*?nodeWorker" {
     const worker: any;
     export default worker;
 }
-declare module "./index.js" {
-    const exported: any;
-    export = exported;
+declare module 'vite-plugin-node-worker' {
+    import { Plugin } from 'vite';
+    function workerPlugin(): Plugin;
+    export default workerPlugin;
 }
