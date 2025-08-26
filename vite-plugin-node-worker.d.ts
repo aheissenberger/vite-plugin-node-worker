93abc6e2483d3cb3941ec089dc7fc14ec0043bb2
Andreas Heissenberger <andreas@heissenberger.at>
2025-08-26 02:53:15 +0200

    fix: update version and repository URLs in package.json; enhance type declarations in vite-plugin-node-worker.d.ts

--- vite-plugin-node-worker.d.ts
+++ vite-plugin-node-worker.d.ts
@@ -1,9 +1,14 @@
-declare module "*?nodeWorker" {
-    const worker: any;
-    export default worker;
+declare module "vite-plugin-node-worker" {
+  const plugin: any;
+  export default plugin;
 }
-declare module 'vite-plugin-node-worker' {
-    import { Plugin } from 'vite';
-    function workerPlugin(): Plugin;
-    export default workerPlugin;
+
+declare module "*?nodeWorker" {
+  const worker: any;
+  export default worker;
+}
+
+declare module "*?modulePath" {
+  const modulePath: string;
+  export default modulePath;
 }
